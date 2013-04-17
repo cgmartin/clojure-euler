aee5c423861b9a9c30aa0a2981cad95ffacd3c3f
Christopher Martin <cgmartin@gmail.com>
2013-04-17 00:36:01 -0400

    Update url in project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euler "0.1.0"
   :description "Project Euler problems in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/cgmartin/clojure-euler"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
